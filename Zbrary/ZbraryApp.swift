f7ad1ec5a8f7f873d86294f895f139c29a7c4a65
Kyle Damon <kyle@ktd4.com>
2022-07-26 16:13:59 -0500

    Initial Commit

---
+++ Zbrary/ZbraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZbraryApp.swift
+//  Zbrary
+//
+//  Created by Kyle Damon on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ZbraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
